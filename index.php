c715ce0287af7200167b22e26b211345b7b70b8d
highcoder <vishnuvanch@gmail.com>
2016-10-04 21:10:18 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>Project 52</title>
+<link rel="stylesheet" href="layout.css"/>
+</head>
+<body>
+	<div class="container">
+		<h2>Project 52</h2>
+		<hr>
+		<table>
+			<tr>
+				<td><strong>Category:</strong></td>
+				<td><input type="text" name="category" placeholder="Category name..."/></td>
+			</tr>
+			<tr>
+				<td><strong>Parent Category:</strong></td>
+				<td>
+					<select name="parent_category">
+						<option>category 1</option>
+						<option>category 2</option>
+						<option>category 3</option>
+						<option>category 4</option>
+						<option>category 5</option>
+						<option>category 6</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2" class="ctr">
+					<input type="button" value="Save" name="add_new_category"/>
+				</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
